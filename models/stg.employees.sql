fd4ca153445641894bdd7b4eea2eb25551fc5d9b
Prajan Gokul Nath <gokulnathprajan@gmail.com>
2025-07-10 18:09:55 +0000

    updated

---
+++ models/stg.employees.sql
@@ -0,0 +1,21 @@
+{{  
+    config(
+        materialized = 'incremental',
+        incremental_strategy = 'append'
+    )
+}}
+
+select 
+EMPLOYEE_ID,
+FIRST_NAME,
+LAST_NAME,
+EMAIL,
+PHONE_NUMBER,
+HIRE_DATE,
+JOB_ID,
+SALARY,
+COMMISSION_PCT,
+MANAGER_ID,
+DEPARTMENT_ID,
+current_timestamp as LOAD_TIME
+from hr.src_employees
